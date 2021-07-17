76371104cd22d703160698311746a6e78bb02027
psilva1982 <severos1982@gmail.com>
2021-07-17 17:09:00 -0300

    db conf

--- conf/settings.py
+++ conf/settings.py
@@ -110,12 +110,8 @@ if os.environ.get('USE_POSTGRES'):
         }
     }
 else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+    default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+    DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 # default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 # DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
